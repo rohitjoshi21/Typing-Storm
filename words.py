a176439a302e4b82cbdb5e3f23e4bd6e0846a71e
Rohit <joshi.rk11166@gmail.com>
2019-07-07 21:25:29 +0545

    Git initialized

---
+++ words.py
@@ -0,0 +1,2 @@
+letter=["cat","dog","cow","tiger","lion","leopard","rabbit","fox","rhinoceros","penguin","peacock","sparrow","parrot","buffalo","apple","rohit","python"\
+        ,"banana","mango","pineapple","papaya","aeroplane","kathmandu","askbuddie","technology","nepal","mountain","hill","terai"]
